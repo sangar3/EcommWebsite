ed20ec7e8136022cdb41db9f8ec522ae6b57fbb6
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-09-06 22:26:09 -0500

    added checkout functionality

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -12,11 +12,22 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
+        # create empty cart  for none-logged in users
         items = []
+        order = {'get_cart_total':0, 'get_cart_items':0 }
 
-    context = {'items':items}
+    context = {'items':items,'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        #create empty cart  for none-logged in users
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items':items,'order':order}
     return render(request, 'store/checkout.html', context)
